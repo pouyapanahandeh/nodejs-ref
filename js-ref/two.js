ced28816900bce4133541d2403133dc02924220e
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-02 21:59:55 +0200

    alert

---
+++ js-ref/two.js
@@ -0,0 +1,5 @@
+// in alert you can have multiple type of variable
+// alert is pop up message thet when you run it,pop up a message include the informations
+
+alert("pooya panahandeh");
+alert(1+3);
