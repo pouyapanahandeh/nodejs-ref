5e1b806fe86ac09ca6d9c5c240ce8f136a835b2b
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-05 15:28:35 +0200

    alert, prompt, confirm

--- js-ref/nine.js
+++ js-ref/nine.js
@@ -4,3 +4,16 @@ alert("This is a pop up message!");
 
 // prompt
 
+let age = prompt('How old are you?', 25);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+// confirm 
+
+// Function confirm shows a modal window with a question and two buttons: OK and CANCEL.
+
+// The result is true if OK is pressed and false otherwise
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
