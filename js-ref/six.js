f815cc8f79385a203204c8ac7861fb323ac6d497
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-05 09:55:36 +0200

    type of

--- js-ref/six.js
+++ js-ref/six.js
@@ -6,3 +6,28 @@ let nameFieldChecked = true;
 // Result will be true or false
 let isGreater = 4 > 1;
 alert(isGreater);
+
+//typeof undefined // "undefined"
+//
+//typeof 0 // "number"
+//
+//typeof true // "boolean"
+//
+//typeof "foo" // "string"
+//
+//typeof Symbol("id") // "symbol"
+//
+//typeof Math // "object"  (1)
+//
+//typeof null // "object"  (2)
+//
+//typeof alert // "function"  (3)
+
+document.write(typeof undefined + "<br>");
+document.write(typeof 0 + "<br>");
+document.write(typeof true + "<br>");
+document.write(typeof "chains" + "<br>");
+document.write(typeof Symbol("id") + "<br>");
+document.write(typeof Math + "<br>");
+document.write(typeof null + "<br>");
+document.write(typeof alert + "<br>");
