5ffdc7421e2428563f6eb69f04c40324a9b5d05f
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-05 09:31:01 +0200

    Boolean variable

---
+++ js-ref/five.js
@@ -0,0 +1,12 @@
+// we have different type of data in programming and in JS
+// at this file we go through the String
+
+// different type of "" '' `` in JS
+
+let strOne = "Hello ";
+let strTwo = " World";
+
+// by `` we can embed 
+let noneSimple = `${strOne} pooya`;
+
+document.write(noneSimple);
---
+++ js-ref/six.js
@@ -0,0 +1,8 @@
+// Boolean type 
+
+let nameFieldChecked = true;
+
+// Boolean values also come as a result of comparisons: 
+// Result will be true or false
+let isGreater = 4 > 1;
+alert(isGreater);
