c8499f46e6c331be23b95228425da54d54dbe13a
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-13 22:53:53 +0200

    conditional part 2

--- js-ref/eleven.js
+++ js-ref/eleven.js
@@ -1,2 +1,8 @@
 // conditional operation part 2
 
+let age = prompt('please enter the your age', '');
+// let result = condition ? value1 : value2
+// write all condition just in one line
+let accessAllowed = (age > 18) ? true : false;
+
+alert(accessAllowed);
