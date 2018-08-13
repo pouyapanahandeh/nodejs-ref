66d46aa5ec201081b8909a0249c771028f5a9725
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-13 09:27:39 +0200

    conditional operation

--- js-ref/ten.js
+++ js-ref/ten.js
@@ -3,3 +3,14 @@
 let year = prompt('In which year was ECMAScript-2015 specification published?', '');
 
 if (year == 2015) alert( 'You are right!' );
+
+// the else condition 
+
+let sal = prompt('what years we are in?','');
+if(sal == 2015){
+	alert('it\'s wrong.');
+} else {
+	alert('How can you be so wrong');
+}
+
+// else if in js
