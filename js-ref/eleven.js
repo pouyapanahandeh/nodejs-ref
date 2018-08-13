7b6de6c614606fc2978fe44a675fe52b9a50817d
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-08-13 23:04:39 +0200

    conditional operation

--- js-ref/eleven.js
+++ js-ref/eleven.js
@@ -1,8 +1,19 @@
 // conditional operation part 2
 
-let age = prompt('please enter the your age', '');
+let sen = prompt('please enter the your age', '');
 // let result = condition ? value1 : value2
 // write all condition just in one line
-let accessAllowed = (age > 18) ? true : false;
+let accessAllowed = (sen > 18) ? true : false;
 
 alert(accessAllowed);
+
+// multiple '?' 
+
+let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+	(age < 18) ? 'Hello!' :
+	(age < 100) ? 'Greetings!' :
+	'What an unusual age!';
+
+alert( message );
