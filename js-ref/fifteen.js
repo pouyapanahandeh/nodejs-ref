7b6a4e1c7d8fdb7554630e9129a0b6a6134e2d66
panahandehpooya <panahandehpooya@gmail.com>
2018-09-23 17:51:51 +0200

    function in js, outer variable

--- js-ref/fifteen.js
+++ js-ref/fifteen.js
@@ -7,3 +7,23 @@ function showMessage() {
 // now we need to call function
 
 showMessage();
+
+//Having variable inside function
+
+function errorMessage() {
+	let messageOne= "Hello world, it's me pooya";
+	alert(messageOne);
+}
+
+errorMessage();
+
+// outer variable(global variable)
+
+let userName = "pooya";
+
+function messageHandling() {
+	let message = "It's me " + userName;
+	alert(message);
+}
+
+messageHandling();
