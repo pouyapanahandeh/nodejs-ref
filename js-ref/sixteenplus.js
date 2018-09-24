88a12ad7ef8f619963907f0a2ef15d1eb7305642
panahandehpooya <panahandehpooya@gmail.com>
2018-09-24 23:07:50 +0200

    object in js 2

--- js-ref/sixteenplus.js
+++ js-ref/sixteenplus.js
@@ -1,2 +1,37 @@
 // Square brackets
 
+let user = {};
+
+// set 
+
+user ["like birds"] = true;
+
+// get
+
+document.write(user["like birds"]);
+
+// delete
+
+delete user["likes bird"];
+
+let userOne = {
+	name : "pooya",
+	age : 26
+};
+
+let key = prompt("what you want to know about user?", "name");
+
+alert(userOne[key]);
+
+// computed properties
+
+let fruit = prompt("Which fruit to buy?", "apple");
+
+let bag = {
+	[fruit]: 5, // the name of the property is taken from the variable fruit
+};
+
+alert( bag.apple );
+
+// existence check
+
