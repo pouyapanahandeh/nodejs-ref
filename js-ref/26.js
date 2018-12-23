56d3a14413a8cf1a2083f4218efbf5a2bebd5eed
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-23 20:04:36 +0100

    Array()

---
+++ js-ref/26.js
@@ -0,0 +1,15 @@
+let arr = new Array("apple", "pear","goje");
+
+document.write(arr);
+
+alert(arr.length)
+
+// Multidimensional arrays
+
+let matrix = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]
+];
+
+alert( matrix[0][1] );
