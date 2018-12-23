fed191703961490cfbff7161bea5f33917ace6a9
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-23 19:07:02 +0100

    pop/push

---
+++ js-ref/23.js
@@ -0,0 +1,35 @@
+// Array in js
+
+let arr = new Array(3);
+
+	arr[0] = "pooya";
+	arr[1] = "Bahar";
+	arr[2] = "Bozorgmehr";
+
+document.write(arr);
+
+// array can be include multiple type value 
+let arrOne = [ 'Apple', { name: 'John' }, true, function() { alert('hello'); } ];
+
+alert(arrOne);
+
+// pop and push in array
+
+//push adds an element to the end.
+//pop takes an element from the end.
+
+let fruits = ["apple", "orange" , "pear"];
+
+alert(fruits.pop());
+
+
+let gadget = ["mobile","transmitor","keyboard"]
+
+alert(gadget);
+
+gadget.push("monitor");
+
+alert(gadget);
+
+
+
