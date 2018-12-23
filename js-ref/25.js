3246138a6880e90943794eb28a8c26de1866d0bc
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-23 19:50:51 +0100

    for..of iterating in js

---
+++ js-ref/25.js
@@ -0,0 +1,16 @@
+// loop in Array()
+
+let life = ["family", "happines", "trust", "change"]
+
+for (i = 0 ; i < life.length ; i++) {
+	alert(life[i])
+}
+
+// short version of for loop
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+// iterates over array elements for..of
+for (let fruit of fruits) {
+	alert( fruit );
+}
