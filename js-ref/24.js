cce3499c82fa6920d5d9f31483d4d5f752914c32
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-23 19:41:48 +0100

    shift\unshift

---
+++ js-ref/24.js
@@ -0,0 +1,12 @@
+// shift and unshift in js
+
+let pooya = ["Hacker", "Programmer","Innovater"];
+
+alert(pooya.shift());
+document.write(pooya)
+
+let zendegi = ["money", "Happines","sex"];
+
+	zendegi.unshift("fighter");
+	
+	document.write( "<br>" + zendegi);
