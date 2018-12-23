49225ee5550fb4c14597ec120016fb70f77c0c66
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-23 17:59:06 +0100

    span string to seperate line

---
+++ js-ref/21.js
@@ -0,0 +1,11 @@
+// function inside the Object()
+
+let person = {
+	name: "pooya",
+	age: 26,
+	sayHi: function() {
+		alert("He is a Good person");
+	}
+};
+
+person.sayHi();
---
+++ js-ref/22.js
@@ -0,0 +1,21 @@
+// write number in different way
+
+function sum(a, b) {
+	return a + b;
+}
+
+// if you wanna use ${} you need to put it with ``
+
+alert(`result ${sum(1,2)}`);
+
+
+// if we use `` we can span  string to multile lines
+
+let guestList = `guests:
+	*pooya
+	*mohammad
+	*parvaneh
+	*masih`;
+	
+document.write(guestList);
+
