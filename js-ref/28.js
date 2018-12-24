8e66d4f64097dc1bf38692430311df72e94ee6d0
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-24 14:08:55 +0100

    searching include

--- js-ref/28.js
+++ js-ref/28.js
@@ -3,3 +3,11 @@
 let arrOne = [1,2,3,4,5]
 	
 	alert(arrOne.concat([6,7]))
+	
+// searching in array 
+
+let arrTwo = ["I","mr","error"]
+
+	// search if array include I
+	alert(arrTwo.includes("I"))
+	
