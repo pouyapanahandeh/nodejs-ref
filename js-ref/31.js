3ed964b59fe1155625fbd2463d833ea2467ba7a4
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-24 21:09:24 +0100

    split and join

---
+++ js-ref/31.js
@@ -0,0 +1,27 @@
+// map in js 
+
+let lengths = ["pooya","hamsaresh","farzandanesh"].map(item => item.length)
+	
+	// map iterate through the elements of array
+	alert(lengths)
+	
+// reverse in array 
+
+let arrOne = [1,2,3,4,5,6,7]
+	
+	// reverse the elements of array
+	document.write(arrOne.reverse())
+	
+// split in js, spilit put string into the array 
+
+let stringOne = "pooya , panahandeh , danial , amir";
+	
+	let arr = stringOne.split(",")
+	document.write("<br>" + arr)
+	
+// join the elements of array
+
+let arrThree = ['no','way','to','have','fun']
+
+	document.write("<br>" + arrThree.join(;))
+	
