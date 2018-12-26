43b39b412a8c41b8688cecb268bfbcb894a64863
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-12-26 23:33:45 +0100

    arrow function tut

---
+++ js-ref/32.js
@@ -0,0 +1,28 @@
+// arrow function in js 
+
+//ES5
+var multiples = function mltpll(x , y) {
+	return x * y;
+}
+
+alert(multiples(4, 4))
+
+//ES6
+
+const zarbdar = (a , b) => {return a * b}
+
+alert(zarbdar(5, 5))
+
+// ES5
+
+let wordSplitter = function splitters(words){
+	return words.split('');
+}
+
+alert(wordSplitter("panahandeh"));
+
+//ES6
+
+const stringSpliter = string => { return string.split('')}
+
+alert(stringSpliter("havaeeam"))
