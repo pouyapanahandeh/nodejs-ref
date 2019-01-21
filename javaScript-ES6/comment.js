aa8f9f9bfd3f0fd402dda95b95008198be4d8c25
pouyapanahandeh <panahandehpooya@gmail.com>
2019-01-21 19:29:15 +0330

    comment in js

---
+++ javaScript-ES6/comment.js
@@ -0,0 +1,10 @@
+ console.log("this is inline comment") // we call it inline bro
+
+ console.log("and below you can see multi line comment")
+
+ /*
+    Dear all, it's
+    multi
+    line
+    comment
+ */
