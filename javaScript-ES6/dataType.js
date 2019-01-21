6b9763de7c9ed47b434167e20cbf6c8b584b6c44
pouyapanahandeh <panahandehpooya@gmail.com>
2019-01-21 23:34:46 +0330

    data types in JS

---
+++ javaScript-ES6/dataType.js
@@ -0,0 +1,12 @@
+/*
+    Data types in js: undefined,null, boolean, string, symbol, number and object.
+*/
+
+var type = "pooya" // define string variable
+
+type = 8
+
+let typeTwo = "panahandeh" // another way to define variable
+
+const typeThree = 3.14 // as you can see we change the type value, but we can not change the const value.
+                      // if we change const value we will get errors.
