f4bec543ecbd6344fc2d62b9c805aeee30249b6c
pouyapanahandeh <panahandehpooya@gmail.com>
2019-01-22 21:47:47 +0330

    initializing, variable case sensitive

---
+++ javaScript-ES6/initializing.js
@@ -0,0 +1,13 @@
+// initializing variable
+
+let a = 10
+
+// declare variable
+let b
+b = 20
+
+
+// variables are case sensitive
+
+let c
+C = 6 // here we get error, because first s is with low letter and the second one is with capital letter
