0b3495339ec159950861f27ae30c3b32a4a858e1
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-11 15:03:48 +0100

    modify array element

--- javaScript-ES6/9.js
+++ javaScript-ES6/9.js
@@ -19,3 +19,11 @@ var secondArray = [1,2,3,4,5,6,7]
 document.write("<br>" + secondArray[0])
 
 document.write("<br>" + secondArray[secondArray.length - 1])
+
+// modify element in array
+
+secondArray[0] = 13
+
+document.write("<br>" + secondArray)
+
+//
