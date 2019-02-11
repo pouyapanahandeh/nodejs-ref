eddb6453fa747cf2a39103dad6e1dd2ad3c5b8b0
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-11 14:17:39 +0100

    skipping string marks

---
+++ javaScript-ES6/6.js
@@ -0,0 +1,17 @@
+// string in js
+
+var myName = "pooya"
+
+document.write(myName + "<br>")
+
+// by adding \ we skip "
+
+var aboutMe = "i am a \"programming student \" at elte"
+
+document.write(aboutMe + "<br>")
+
+// we can put our string in ' ' instead " " to skip " in middle of string
+
+var skipper = ' pooya is a normal "programmer" and he can make it'
+
+document.write(skipper)
