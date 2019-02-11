1c3d07f1da877ad0f23d8cb9aed3ff6dd720de44
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-11 13:50:31 +0100

    freecodecamp js tutorila

---
+++ javaScript-ES6/1.js
@@ -0,0 +1,17 @@
+// javaScript recap 
+
+/* js data variable : undefined, null, boolean, string, symbol, number, and object */
+
+var a = "pooya"
+
+document.write(a)
+
+var a = 8
+
+document.write( "<br>" + a)
+
+/* you can change the data of var in different position */
+
+let b = 26	// let is like var, there is a different, let is local and var is global
+
+const pi = 3.14 // you can not change the value of const like var because it's constant
---
+++ javaScript-ES6/2.js
@@ -0,0 +1,9 @@
+ var a = 8
+
+document.write(a + "<br>")
+
+a = 16
+
+document.write(a)
+
+// as you can see, for changing the value of var, you do not need to write whole line again, you simply can write the name and new value.
