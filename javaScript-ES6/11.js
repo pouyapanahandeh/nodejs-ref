9ca21893d038f592ac7ab57145777e3e3f8fdd10
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-13 15:59:38 +0100

    stand line

--- javaScript-ES6/11.js
+++ javaScript-ES6/11.js
@@ -28,4 +28,25 @@ document.write(minusSeven(14))
 
 document.write("<br>" + "-----------------" + "<br>")
 
+// stand in line
+
+function nextInLine(arr, item) {
+	
+	arr.push(item)
+	return arr.shift()
+
+}
+
+var testArr = [1,2,3,4,5]
+
+// by JSON.stringify we print our array as a string
+document.write("Before anything happen " + JSON.stringify(testArr) + "<br>")
+// according the function we have, here by the push function we add 6 to our array
+document.write(nextInLine(testArr, 6) + "<br>")
+// now we print whole array after all function operation
+document.write("After the function operation" + JSON.stringify(testArr) + "<br>")
+
+document.write("<br>" + "-----------------" + "<br>")
+
+
 
