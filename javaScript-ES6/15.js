83ba05a9d64271d7e8adffe98609c0dfee79dc01
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-22 23:39:04 +0100

    object in js

--- javaScript-ES6/15.js
+++ javaScript-ES6/15.js
@@ -1,2 +1,15 @@
 // object in js 
 
+var objTest = {
+	"name": "pooya",
+	"lastName": "panahandeh",
+	"age": 26,
+	"friends": ["mahdi","amir","mohammad"]
+};
+
+var acsOne = objTest.name;
+document.write(acsOne + "<br>")
+
+// or
+
+document.write(objTest.lastName)
