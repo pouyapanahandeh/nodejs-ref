bdb64b35e0183a27fe8c5a19b2cb11faca1f0552
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-24 21:43:48 +0100

    access to element in nested array

---
+++ javaScript-ES6/16.js
@@ -0,0 +1,24 @@
+// accessing the nested array
+
+var myPlants = [
+	{
+	
+	types : "flowers",
+	list : [
+		"rose",
+		"tulip",
+		"dandelion"
+		]	
+	},
+	{
+		type : "trees",
+		list : [
+			"fir",
+			"pine",
+			"brich"
+		]
+	}
+];
+
+document.write(myPlants[1].list[1])
+
