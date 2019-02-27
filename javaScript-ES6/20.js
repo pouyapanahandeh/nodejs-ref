f5429be71ec2c3de5ed619f48ea22449690ab20d
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-27 20:11:34 +0100

    arrow function in js

---
+++ javaScript-ES6/20.js
@@ -0,0 +1,17 @@
+//arrow function in js
+
+var magic = function () {
+	return new Date()
+}
+
+document.write(magic() + "<br>")
+
+// now we change it to arrow function
+
+var magic2 =  () => new Date()
+
+document.write(magic2() + "<br>")
+
+
+// arrow function 
+
