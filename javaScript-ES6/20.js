0531707e205757cb6991b3703342ad9889b3709f
pouyapanahandeh <panahandehpooya@gmail.com>
2019-02-27 21:13:46 +0100

    arrow function in js

--- javaScript-ES6/20.js
+++ javaScript-ES6/20.js
@@ -13,5 +13,20 @@ var magic2 =  () => new Date()
 document.write(magic2() + "<br>")
 
 
-// arrow function 
+// normal function with parameters function 
 
+var concatOne = function(arrOne, arrTwo){
+	return arrOne.concat(arrTwo)
+}
+
+document.write([1,2,3], [,4,5,6] + "<br>")
+
+// now we change it to arrow function 
+
+var concatTwo = (arOne, arTwo) => arOne.concat(arTwo)
+
+document.write([10,11,12], [,13,14,15] + "<br>")
+
+document.write(concatTwo([10,11,12], [13,14,15] + "<br>")) 
+
+// as you can see both method is work, but with second method we do not need to do any modification
