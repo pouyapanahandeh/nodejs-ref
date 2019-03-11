c99ba2675fde45d54d043f6bccd64c4b7ff6db08
pouyapanahandeh <panahandehpooya@gmail.com>
2019-03-11 10:48:59 +0100

    map in js include built in method

---
+++ javaScript-ES6/mapTwo.js
@@ -0,0 +1,26 @@
+let map = new Map() // create the map
+//.     key.  object
+map.set('1', 'str1')
+map.set(1, 'num1')
+map.set(true, 'bool1')
+
+// key are not convert to String, but in object key are convert to string
+// map keeps the types so these these two are different
+
+alert(map.get('1'))
+alert(map.get(1))
+
+// check the size of map 
+
+document.write("The size of map: " + map.size + "<br>")
+
+// map can use object as key
+//. object
+let John = {name: "John"}
+
+let visitCounterMap = new Map()
+
+visitCounterMap.set(John, 123)
+
+document.write(visitCounterMap.get(John) + "<br>")
+
